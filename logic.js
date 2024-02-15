381dcc03d68f6c17ec297b66bc02df4510c7a100
Ingrid G <ing.gape18@gmail.com>
2024-02-14 19:36:23 -0500

    filtrando resultados

--- logic.js
+++ logic.js
@@ -1,1 +1,7 @@
-export const filter = () =>{console.log('la importacion de funciones esta ok')}
+// Función para filtrar canciones usando una expresión regular
+export const filterSongsByRegex = (array, regex) => {
+    return array.filter(element => regex.test(element));
+};
+
+
+export const filter = () => { console.log('la importacion de funciones esta ok') }
--- main.js
+++ main.js
@@ -1,23 +1,47 @@
-import { filter } from '/logic.js'
+import { filter,  filterSongsByRegex  } from '/logic.js';
 
-let songs = ["song1", "song2", "song3", "song4", "song5"]
+let songs = ["song1", "song2", "song3", "song4", "song5"];
 
 const search = document.getElementById('search');
+const searchBtn = document.getElementById('searchBtn'); 
 const resultsContainer = document.getElementById('results');
 const playlistContainer = document.getElementById('playlist');
 const favoritesContainer = document.getElementById('favorites');
 
-filter()
-
+filter();
+//Imprimir canciones
 songs.forEach(
     song => {
         resultsContainer.innerHTML += `
             <li class="song">
-                <p>'${'hiii'}'</p>
+                <p>${song}</p>
                 <button>play</button>
                 <button>like</button>
                 <button>remove</button>
             </li >
         `
     }
-)
+);
+
+//Filtrar busqueda
+searchBtn.addEventListener('click', () => {
+    let busqueda = search.value;
+   
+    const regExp = new RegExp(busqueda,'i');
+    let cancionesFiltradas = filterSongsByRegex(songs, regExp);
+    console.log(cancionesFiltradas)
+    
+    // Limpiamos el contenedor de resultados antes de agregar nuevos elementos
+    resultsContainer.innerHTML = '';
+
+    cancionesFiltradas.forEach(song => {
+        resultsContainer.innerHTML += `
+            <li class="song">
+                <p>${song}</p>
+                <button>play</button>
+                <button>like</button>
+                <button>remove</button>
+            </li>
+        `;
+    });
+});
