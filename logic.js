1c191524c2a8326fe90f7ba904bdb7e2332456c3
Ingrid G <ing.gape18@gmail.com>
2024-02-20 19:59:08 -0500

    logica

--- logic.js
+++ logic.js
@@ -3,5 +3,150 @@ export const filterSongsByRegex = (array, regex) => {
     return array.filter(element => regex.test(element));
 };
 
-
 export const filter = () => { console.log('la importacion de funciones esta ok') }
+
+class Song {
+    constructor(nombre, author, duracion, album, año, genero, letra, cover, urlSong) {
+        this.nombre = nombre;
+        this.author = author;
+        this.duracion = duracion;
+        this.album = album;
+        this.año = año;
+        this.genero = genero;
+        this.letra = letra;
+        this.cover = cover;
+        this.urlSong = urlSong;
+    }
+
+    getSongNameAndAuthor(){
+        return `${this.nombre}` + `${this.author}` 
+    }
+
+    getSongNameAndDuration(){
+        return `${this.nombre}` + `${this.duracion}` 
+    }
+
+    getSongNameAndAlbum() {
+        return `${this.nombre}` + `${this.album}`  
+    }
+
+    getSongNameAndYear(){
+        return `${this.nombre}` + `${this.año}` 
+    }
+
+    getSongNameAndCover(){
+        return `${this.nombre}` + `${this.cover}` 
+    }
+
+    getSongUrlSong(){
+        return `${this.nombre}` + `${this.urlSong}` 
+    }
+}
+
+
+class Playlist{
+    constructor(nombre, listaCanciones, ordenEscucha) {
+        this.nombre = nombre;
+        this.listaCanciones = listaCanciones;
+        this.ordenordenEscucha =ordenEscucha;
+    }
+
+    getPlaylistName() {
+        return `${this.nombre}`
+    }
+
+    getPlaylistSongs() {
+        return `${this.listaCanciones}`
+    }
+
+    getPlaylistListeningOrder() {
+        return `${this.ordenordenEscucha}`
+    }
+
+    addSongToPlaylist(){
+         this.listaCanciones.push(song)
+    }
+
+    removeSongFromPlaylist() {
+        this.listaCanciones=this.listaCanciones.filter(s=>s!==song) 
+    }
+
+    playPlaylist() {
+        this.listaCanciones.forEach(
+            song => {
+                console.log('playing: ' + song.nombre)
+            }
+        );
+    }
+
+    getCurrentSong(i) {
+        return this.listaCanciones[i]
+    }
+
+    nextSong() {
+        return this.listaCanciones.shift()
+
+    }
+};
+
+class Reproductor {
+    catalogodeCanciones;
+    currentSong;
+    currentPlaylist;
+    constructor() {
+        this.catalogodeCanciones = [
+            newSong(nombre1, author1, duracion1, album1, año1, genero1, letra1, cover1, '/assets/songs/1.mp3'),
+            newSong(nombre2, author2, duracion2, album2, año2, genero2, letra2, cover2, '/assets/songs/2.mp3'),
+            newSong(nombre3, author3, duracion3, album3, año3, genero3, letra3, cover3, '/assets/songs/3.mp3'),
+            newSong(nombre4, author4, duracion4, album4, año4, genero4, letra4, cover4, '/assets/songs/4.mp3'),
+            newSong(nombre5, author5, duracion5, album5, año5, genero5, letra5, cover5, '/assets/songs/5.mp3'),
+        ] 
+    };
+
+    //Metodo que muestra los nombres de canciones
+    mostrarCanciones() {
+        let canciones = document.getElementById()
+        this.catalogodeCanciones.forEach(song =>
+            canciones.innerHTML += `<p class='cancion'>${song.nombre}</p>`
+        )
+    };
+
+    //Metodo que busca una cancion por su nombre
+    buscarCancion(songName) {
+        return this.catalogodeCanciones.find(song => song.name === songName)
+    };
+
+
+
+    //Reproducir cancion
+    play() {
+        let audio = new Audio(this.currentSong.urlSong)
+        audio.play()
+    };
+
+
+    //Pausar cancion
+    pause() {
+        let pauseBtn = document.getElementById('pause')
+        pauseBtn.addEventListener('click', () => {
+            let currentSong = this.getCurrentSong()
+            let audio = new Audio(urrentSong.urlSong)
+            audio.pause()
+        })
+    };
+
+    //Detener cancion
+    stop() {
+        let stopBtn = document.getElementById('stop')
+        stopBtn.addEventListener('click', () => {
+            let currentSong = this.getCurrentSong()
+            let audio = new Audio(urrentSong.urlSong)
+            audio.pause()
+            audio.currentTime = 0;
+        })
+    };
+    
+}
+
+/* let reproductor = new Reproductor();
+console.log(reproductor.buscarCancion(songName, 'cancion1')) */
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import { filter,  filterSongsByRegex  } from '/logic.js';
+import { filter, filterSongsByRegex  } from '/logic.js';
+
 
 let songs = ["song1", "song2", "song3", "song4", "song5"];
 
@@ -45,3 +46,78 @@ searchBtn.addEventListener('click', () => {
         `;
     });
 });
+
+
+
+
+/*next button
+const nextButton = document.getElementById('next');
+
+const songs = ['https://cdn.pixabay.com/audio/2021/11/01/audio_00fa5593f3.mp3',
+  'https://cdn.pixabay.com/audio/2022/02/22/audio_c06fba1b22.mp3',
+  'https://cdn.pixabay.com/audio/2021/10/22/audio_6642827492.mp3',
+  'https://cdn.pixabay.com/audio/2022/04/12/audio_13feea552b.mp3',
+  'https://cdn.pixabay.com/audio/2021/11/20/audio_a69b88e9f5.mp3'
+]
+
+let audio = new Audio()
+
+nextButton.addEventListener('click', changeAudio);
+audio.addEventListener('ended', changeAudio);
+
+let index = 0
+
+function changeAudio() {
+  if (!audio.src) {
+    audio.src = songs[index];
+    audio.play();
+  } else {
+    if (index === songs.length - 1) {
+      index = 0;
+    } else {
+      index++
+    }
+    audio.src = songs[index];
+    audio.play()
+  }
+
+}*/
+
+/*const audio = new Audio('https://cdn.pixabay.com/audio/2024/01/15/audio_9914e58808.mp3')
+audio.volume = 0.1
+audio.currentTime = 245
+
+const input = document.getElementById('input')
+const search = document.getElementById('search')
+const play = document.getElementById('play')
+const pause = document.getElementById('pause')
+const mute = document.getElementById('mute')
+const sound = document.getElementById('sound')
+
+play.addEventListener('click', () => {
+  audio.play()
+})
+
+pause.addEventListener('click', () => {
+  audio.pause()
+})
+
+mute.addEventListener('click', () => {
+  audio.volume = 0
+})
+
+sound.addEventListener('click', () => {
+  audio.volume = 0.1
+})
+
+search.addEventListener('click', () => {
+  audio.src = input.value
+})
+
+audio.addEventListener('ended', () => {
+  alert('termino el audio, paso al siguiente')
+  audio.src = 'https://cdn.pixabay.com/audio/2024/01/16/audio_e2b992254f.mp3'
+  audio.play()
+})
+
+ */
